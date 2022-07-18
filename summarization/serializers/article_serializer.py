e75dc2be1929f12669b5291297911da4bc5834f2
Botond <b.botond25@gmail.com>
2022-07-18 14:25:11 +0200

    Added parser classes and warc processing script

---
+++ summarization/serializers/article_serializer.py
@@ -0,0 +1,16 @@
+import gzip
+import os.path
+from abc import ABC
+from typing import List
+
+from models.article import Article
+
+
+class ArticleSerializer(ABC):
+    @staticmethod
+    def serialize_articles(directory: str, news_page_name: str, articles: List[Article]):
+        file = os.path.join(directory, f'{news_page_name}.jsonl.gz')
+        with gzip.open(file, "w") as outfile:
+            for article in articles:
+                outfile.write(article.to_json().encode())
+                outfile.write('\n'.encode())
