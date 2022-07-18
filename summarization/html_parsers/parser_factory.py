20233573a35da718670c3ec8b71e17895736506f
Botond <b.botond25@gmail.com>
2022-07-18 14:49:18 +0200

    fixed index parser

--- summarization/html_parsers/parser_factory.py
+++ summarization/html_parsers/parser_factory.py
@@ -1,4 +1,5 @@
 from html_parsers.hvg_parser import HVGParser
+from html_parsers.index_parser import IndexParser
 from html_parsers.napi_parser import NapiParser
 from html_parsers.nepszava_parser import NepszavaParser
 from html_parsers.parser_base import ParserBase
@@ -9,7 +10,7 @@ from html_parsers.twentyfour_parser import TwentyFourParser
 class HtmlParserFactory:
     parsers = {
         'telex': lambda: TelexParser(),
-        'index': lambda: TelexParser(),
+        'index': lambda: IndexParser(),
         'napi': lambda: NapiParser(),
         'hvg': lambda: HVGParser(),
         '24': lambda: TwentyFourParser(),
