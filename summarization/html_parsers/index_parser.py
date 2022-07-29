38875ec3a9196022b107c699e761ba5f520c3204
Botond <b.botond25@gmail.com>
2022-07-29 10:21:31 +0200

    Added more html tags to remove from sites and strip article

--- summarization/html_parsers/index_parser.py
+++ summarization/html_parsers/index_parser.py
@@ -19,7 +19,7 @@ class IndexParser(ParserBase):
     def get_article_text(self, url, soup) -> str:
         article = soup.find('div', class_="cikk-torzs")
         assert_has_article(article, url)
-        return article.text
+        return article.text.strip()
 
     def get_tags(self, soup) -> Set[str]:
         tags_ul = soup.find('ul', class_="cikk-cimkek")
@@ -30,8 +30,17 @@ class IndexParser(ParserBase):
     def remove_captions(self, soup) -> BeautifulSoup:
         to_remove = []
         to_remove.extend(soup.find_all('div', class_='cikk-bottom-text-ad'))
-        to_remove.extend(soup.find_all('div', class_='kepala'))
+        to_remove.extend(soup.find_all('div', class_='szerkfotogallery'))
+        to_remove.extend(soup.find_all('div', class_='szerkfotoimage'))
+
+        to_remove.extend(soup.find_all('div', class_='keretes-donate-doboz'))
+        to_remove.extend(soup.find_all('aside', class_='m-automatic-file-snippet'))
+        to_remove.extend(soup.find_all('div', class_='m-kepkuldes-box'))
+        to_remove.extend(soup.find_all('div', class_='nm_supported__wrapper'))
         to_remove.extend(soup.find_all('div', class_='photographer'))
+
+        to_remove.extend(soup.find_all('div', class_='indavideo'))
+        to_remove.extend(soup.find_all('div', id='socialbox_facebook'))
         to_remove.extend(soup.find_all('blockquote', class_='twitter-tweet'))
         to_remove.extend(soup.find_all('blockquote', class_='tiktok-embed'))
         for r in to_remove:
