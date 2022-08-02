a3d13897d8ed38b37ae1694259735edc832e54d7
Botond <b.botond25@gmail.com>
2022-08-02 11:47:17 +0200

    added more captions to remove and fixed tags

--- summarization/html_parsers/nlc_parser.py
+++ summarization/html_parsers/nlc_parser.py
@@ -1,35 +1,47 @@
-from typing import Set, Optional
+from typing import Optional, Set
 
 from bs4 import BeautifulSoup
 
 from summarization.html_parsers.parser_base import ParserBase
-from summarization.utils.assertion import assert_has_title, assert_has_article
+from summarization.utils.assertion import assert_has_article, assert_has_title
 
 
 class NLCParser(ParserBase):
     def get_title(self, url, soup) -> str:
-        title = soup.find('h1', class_="o-post__title")
+        title = soup.find('h1', class_='o-post__title')
+        if title is None:
+            # old css class
+            title = soup.find('h2', class_='o-post__title')
         assert_has_title(title, url)
         return title.text.strip()
 
     def get_lead(self, soup) -> Optional[str]:
-        lead = soup.find('div', class_="o-post__lead")
+        lead = soup.find('div', class_='o-post__lead')
         return "" if lead is None else lead.text.strip()
 
     def get_article_text(self, url, soup) -> str:
-        article = soup.find('div', class_="u-onlyArticlePages")
+        article = soup.find('div', class_='u-onlyArticlePages')
         assert_has_article(article, url)
-        return article.text
+        return article.text.strip()
 
     def get_tags(self, soup) -> Set[str]:
-        tags_ul = soup.find('ul', class_="cikk-cimkek")
-        if tags_ul:
-            return set(c.text for c in tags_ul.children)
+        tags = soup.find('div', class_='single-post-tags')
+        if tags:
+            return set(t for t in tags.text.strip().split('\n'))
         return set()
 
     def remove_captions(self, soup) -> BeautifulSoup:
         to_remove = []
+        to_remove.extend(soup.find_all('div', class_='o-post__authorWrap'))
+        to_remove.extend(soup.find_all('div', class_='cikkkeptable'))
+        to_remove.extend(soup.find_all('table', class_='cikkkeptable'))
+        to_remove.extend(soup.find_all('table', class_='tabla_babazzunk'))
+        to_remove.extend(soup.find_all('div', class_='banner-container'))
+        to_remove.extend(soup.find_all('div', class_='u-sponsoredBottom'))
+        to_remove.extend(soup.find_all('div', class_='wp-caption'))
+        to_remove.extend(soup.find_all('div', class_='m-relatedWidget'))
+        to_remove.extend(soup.find_all('div', class_='o-cegPostCnt'))
+        to_remove.extend(soup.find_all('div', class_='recipe-wrapper'))
         for r in to_remove:
             r.decompose()
         return soup
-
