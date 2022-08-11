ccfbfd6b9f8b5b63c5f71baf0cd5cb6291cc6c24
Botond <b.botond25@gmail.com>
2022-08-11 10:56:42 +0200

    Added origo parser

--- summarization/errors/invalid_page_error.py
+++ summarization/errors/invalid_page_error.py
@@ -3,5 +3,5 @@ from summarization.errors.page_error import PageError
 
 class InvalidPageError(PageError):
     def __init__(self, url, msg=''):
-        message = f'Invalid page: {url}\n{msg}'
+        message = f'Invalid page ({msg}): {url}'
         super().__init__(message)
---
+++ summarization/html_parsers/origo_parser.py
@@ -0,0 +1,144 @@
+import copy
+from datetime import datetime
+from typing import Optional, Set
+
+import dateparser
+from bs4 import BeautifulSoup, Tag
+
+from summarization.errors.invalid_page_error import InvalidPageError
+from summarization.html_parsers.parser_base import ParserBase
+from summarization.utils.assertion import assert_has_article, assert_has_title
+
+
+class OrigoParser(ParserBase):
+    def check_page_is_valid(self, url, soup):
+        if soup.select('body.gallery') or soup.select('body.page-gallery'):
+            raise InvalidPageError(url, 'Gallery')
+
+    def get_title(self, url, soup) -> str:
+        title = soup.find('h1', class_='article-title')
+
+        if not title:
+            title = soup.find('h1', class_='cikk-cim')
+
+        if not title:
+            title = soup.find('div', class_='title')
+
+        if not title:
+            title = soup.find('span', class_='cim')
+
+        if not title:
+            title = next(iter(soup.select('header#article-head > h1')), None)
+
+        if not title:
+            title = next(iter(soup.select('div#article-head > h1')), None)
+
+        if not title:
+            title = next(iter(soup.select('div.article_head > h1')), None)
+
+        assert_has_title(title, url)
+        return title.get_text(' ').strip()
+
+    def get_lead(self, soup) -> Optional[str]:
+        lead = soup.find('div', id='leades')
+
+        if not lead:
+            lead = soup.find('div', class_='article-lead')
+
+        if not lead:
+            lead = soup.find('td', id='lead')
+
+        return "" if lead is None else lead.get_text(' ').strip()
+
+    def get_article_text(self, url, soup) -> str:
+        article = soup.find('div', id='kenyer-szov')
+
+        if not article:
+            article = soup.find('td', id='cikktest')
+
+        if not article:
+            article = soup.find('div', class_='article-content')
+
+        if not article:
+            article = copy.copy(soup.find('div', id='article-text'))
+            if article and article.select('div.article-lead'):
+                article.select('div.article-lead')[0].decompose()
+
+        if not article:
+            article = copy.copy(soup.find('article', id='article-text'))
+            if article and article.select('div.article-lead'):
+                article.select('div.article-lead')[0].decompose()
+
+        assert_has_article(article, url)
+        return article.get_text(' ').strip()
+
+    def get_date_of_creation(self, soup) -> Optional[datetime]:
+        date = soup.find('span', class_='cikk-datum')
+
+        if not date:
+            date = soup.find('div', class_='date')
+
+        if not date:
+            date = soup.find('td', class_='cikkdatum')
+            if date:
+                return dateparser.parse(date.get_text(' ').split('|')[0].strip())
+
+        if not date:
+            date = soup.find('div', class_='article-date')
+
+        if not date:
+            date = soup.find('span', id='article-date')
+
+        if not date:
+            return None
+
+        return dateparser.parse(date.get_text(' '))
+
+    def get_tags(self, soup) -> Set[str]:
+        tags = soup.select('div.article-tags a')
+        if tags:
+            return set([a.text for a in tags])
+
+        tag_kozep = soup.find('div', id='kozep')
+        if tag_kozep:
+            tags = [tag.text.strip() for tag in tag_kozep if type(tag) == Tag and tag.name == 'a']
+            if tags:
+                return set(tags)
+
+        kapcs_cimke = soup.find('div', id='kapcs-cimke', class_='kapcs-cimke')
+        if kapcs_cimke:
+            tags = [tag.text.strip() for tag in kapcs_cimke if type(tag) == Tag and tag.name == 'a']
+            if tags:
+                return set(tags)
+
+        rovat_cimke = soup.find('div', id='rovatcimkek')
+        if rovat_cimke:
+            tags = [tag.text.strip() for tag in rovat_cimke if type(tag) == Tag and tag.name == 'a']
+            if tags:
+                return set(tags)
+
+        meta = soup.find('div', class_='article-meta')
+        if meta:
+            tags = [tag.text.strip() for tag in meta if type(tag) == Tag and tag.name == 'a']
+            if tags:
+                return set(tags)
+        return set()
+
+    def remove_captions(self, soup) -> BeautifulSoup:
+        to_remove = []
+        to_remove.extend(soup.find_all('div', id='multidoboz'))
+        to_remove.extend(soup.find_all('embedobject', type='lifehukapcscikk'))
+        # instagram and maybe more
+        to_remove.extend(soup.find_all('div', class_='cikk-obj'))
+        to_remove.extend(soup.find_all('div', id='article-related'))
+        to_remove.extend(soup.find_all('div', class_='img-holder'))
+        to_remove.extend(soup.find_all('figure', class_='newpic'))
+        # pics
+        to_remove.extend([img.parent for img in soup.select('p[align=center] img')])
+        # explanatory texts at the end of the articles
+        to_remove.extend(soup.find_all('table', class_='szerk-table'))
+        to_remove.extend(soup.find_all('span', class_='source'))
+
+        for r in to_remove:
+            r.decompose()
+        return soup
