4751e166fc0e29a94a86ede31008df2c28247268
dorinapetra <dorinapetra@gmail.com>
2022-08-22 12:16:43 +0200

    fix naming

--- summarization/html_parsers/filters/image_filter.py
+++ summarization/html_parsers/filters/image_filter.py
@@ -1,10 +1,10 @@
 from panflute import Image, toJSONFilters
 
 
-def increase_header_level(elem, doc):
+def filter_images(elem, doc):
     if type(elem) == Image:
         return []
 
 
 if __name__ == "__main__":
-    toJSONFilters([increase_header_level])
+    toJSONFilters([filter_images])
