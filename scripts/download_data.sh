e4efa7c8c4c305837eadbea1626387f90e1b39d4
dorinapetra <dorinapetra@gmail.com>
2022-08-23 10:25:34 +0200

    rename output path

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -6,7 +6,7 @@ CC_CORPUS_PATH="$2"
 CC_CORPUS_SCRIPT_PATH=$CC_CORPUS_PATH/scripts
 ALLOWED_MIMES_PATH=$CC_CORPUS_PATH/allowed_mimes.txt
 
-CC_DIR_PATH="$3"
+CC_OUTPUT_PATH="$3"
 
 while read -r LINE
 do
@@ -19,26 +19,26 @@ do
 
   python "$CC_CORPUS_SCRIPT_PATH"/get_indexfiles.py \
     -q $URL \
-    -o "$CC_DIR_PATH/$SITE/cc_index" \
+    -o "$CC_OUTPUT_PATH/$SITE/cc_index" \
     -l "$SITE.log" \
     -m 5
 
   echo 'Filtering index'
   python "$CC_CORPUS_SCRIPT_PATH"/filter_index.py \
-    "$CC_DIR_PATH/$SITE/cc_index/" \
-    "$CC_DIR_PATH/$SITE/cc_index_filtered/" \
+    "$CC_OUTPUT_PATH/$SITE/cc_index/" \
+    "$CC_OUTPUT_PATH/$SITE/cc_index_filtered/" \
     -a "$ALLOWED_MIMES_PATH" \
     -P 12
 
   echo 'Deduplicating index'
   python "$CC_CORPUS_SCRIPT_PATH"/deduplicate_index_urls.py \
-    -i "$CC_DIR_PATH/$SITE/cc_index_filtered/" \
-    -o "$CC_DIR_PATH/$SITE/cc_index_dedup/"
+    -i "$CC_OUTPUT_PATH/$SITE/cc_index_filtered/" \
+    -o "$CC_OUTPUT_PATH/$SITE/cc_index_dedup/"
 
   echo 'Downloading pages'
   python "$CC_CORPUS_SCRIPT_PATH"/download_pages.py \
-    -o "$CC_DIR_PATH/$SITE/cc_downloaded" \
+    -o "$CC_OUTPUT_PATH/$SITE/cc_downloaded" \
     -e warc.gz \
-    -i "$CC_DIR_PATH/$SITE/cc_index_dedup/*.gz"
+    -i "$CC_OUTPUT_PATH/$SITE/cc_index_dedup/*.gz"
 
 done < "$1"
