351d99128e3e5d9b8738b3b27963a636a99a82a2
Bart <b.botond25@gmail.com>
2022-09-07 15:58:13 +0200

    process sites one by one

--- summarization/preprocess/preprocessor.py
+++ summarization/preprocess/preprocessor.py
@@ -1,4 +1,6 @@
 import glob
+import os
+from distutils.util import strtobool
 from multiprocessing import cpu_count
 
 import pandas as pd
@@ -8,6 +10,7 @@ from lsh.minhash import MinHasher
 from summarization.preprocess.language_detector import LanguageDetector
 from summarization.utils.config_reader import get_config_from_yaml
 from summarization.utils.data_helpers import parallelize_df_processing
+from summarization.utils.dateparser import DateParser
 from summarization.utils.tokenizer import Tokenizer
 
 
@@ -22,62 +25,76 @@ class Preprocessor:
 
     def preprocess(self):
         sites = glob.glob(f'{self.config.src_dir}/*.jsonl.gz')
-        df_sites = [pd.read_json(f'{site}', lines=True) for site in sites]
+        site_domains = [site.replace('.jsonl.gz', '').replace(f'{self.config.src_dir}/', '')
+                        for site in glob.glob('/home/bart/data/*.jsonl.gz')]
 
         language_detector = LanguageDetector(model_path=self.config.lang_detector_model_path)
 
-        # drop non-Hungarian sentences
-        df_sites = [df[df['article']
-                       .apply(lambda x: x.replace('\n', ' '))
-                       .map(language_detector.predict) == 'hu']
-                    for df in df_sites]
+        for site in sites:
+            df_site = pd.read_json(f'{site}', lines=True)
 
-        df_sites = [df[df['article'].str.len() > self.config.min_article_len] for df in df_sites]
-        df_sites = [df.drop_duplicates(subset=['article']) for df in df_sites]
+            df_site = self._drop_non_hungarian_sentences(df_site, language_detector)
+            df_site = df_site[df_site['article'].str.len() > self.config.min_article_len]
+            df_site = parallelize_df_processing(df_site, self.filter_by_number_of_sentences, cpu_count() // 2, 100)
 
-        # filter articles by number of sentences
-        df_sites = [parallelize_df_processing(df, self.filter_by_number_of_sentences, cpu_count() // 2, 100)
-                    for df in df_sites]
+            # add fingerprint column to df
+            df_site = parallelize_df_processing(df_site, self.create_fingerprints, cpu_count() // 2, 100)
 
-        # add fingerprint column to dfs
-        df_sites = [parallelize_df_processing(df, self.create_fingerprints, cpu_count() // 2, 100) for df in df_sites]
+            self._add_fingerprints_to_lsh(df_site)
 
-        self._add_fingerprints_to_lsh(df_sites)
+            # saving temporary file
+            self._temporarily_save_site(df_site)
 
-        df_sites = self._remove_duplicates(df_sites)
-        df_sites = [df.drop('fingerprint', axis=1) for df in df_sites]
+        duplicates_to_drop = self._get_duplicates_to_drop(site_domains)
 
-        # saving the deduplicated dataframes
-        for df in df_sites:
-            site = df.iloc[0].domain.split('.')[0]
-            df.to_json(f'{self.config.out_dir}/{site}_dedup.jsonl.gz', orient='records', lines=True, compression='gzip')
+        # remove duplicates from the temporary files and remove them from disk
+        for (domain, drops) in duplicates_to_drop.items():
+            df_site = pd.read_json(f'{self.config.src_dir}/{domain}_temp.jsonl.gz', lines=True)
 
-    def _remove_duplicates(self, dfs):
-        drops = {f'{i}': [] for i in range(len(dfs))}
+            df_site = df_site[~df_site.uuid.isin(drops)]
+            df_site.to_json(f'{self.config.out_dir}/{domain}_dedup.jsonl.gz', orient='records',
+                            lines=True, compression='gzip')
+
+            os.remove(f'{self.config.src_dir}/{domain}_temp.jsonl.gz')
+
+    def _temporarily_save_site(self, df):
+        df_site = df.drop('fingerprint', axis=1)
+        domain = self._get_domain_of_site(df_site)
+        df_site.to_json(f'{self.config.src_dir}/{domain}_temp.jsonl.gz', orient='records',
+                        lines=True, compression='gzip')
+
+    def _get_domain_of_site(self, df):
+        return df.iloc[0].domain.split('.')[0]
+
+    def _drop_non_hungarian_sentences(self, df, language_detector):
+        return df[df['article']
+                  .apply(lambda x: x.replace('\n', ' '))
+                  .map(language_detector.predict) == 'hu']
+
+    def _get_duplicates_to_drop(self, site_domains):
+        drops = {f'{domain}': [] for domain in site_domains}
 
         duplicates = self.lsh.get_all_duplicates(min_jaccard=self.config.min_jaccard)
 
         for (left, right) in duplicates:
-            (left_df, _, left_loc) = left.partition('_')
-            (right_df, _, right_loc) = right.partition('_')
-            left_row = dfs[int(left_df)].iloc[int(left_loc)]
-            right_row = dfs[int(right_df)].iloc[int(right_loc)]
+            left_domain, left_uuid, left_date, left_has_lead = left.split('_')
+            right_domain, right_uuid, right_date, right_has_lead = right.split('_')
 
             # drop the one with empty lead or earlier crawl time
-            drop = (left_df, left_row.name) if left_row.lead == '' and right_row.lead != '' \
-                else (right_df, right_row.name) if right_row.lead == '' and left_row.lead != '' \
-                else (left_df, left_row.name) if left_row.cc_date < right_row.cc_date \
-                else (right_df, right_row.name)
-            drops[drop[0]].append(int(drop[1]))
+            drop = (left_domain, left_uuid) if strtobool(left_has_lead) and not strtobool(right_has_lead) \
+                else (right_domain, right_uuid) if strtobool(right_has_lead) and not strtobool(left_has_lead) \
+                else (left_domain, left_uuid) if DateParser.parse(left_date) < DateParser.parse(right_date) \
+                else (right_domain, right_uuid)
+            drops[drop[0]].append(drop[1])
 
-        for df_num, df_drops in drops.items():
-            dfs[int(df_num)].drop(df_drops, inplace=True)
-        return dfs
+        return drops
 
-    def _add_fingerprints_to_lsh(self, dfs):
-        for i, df in enumerate(dfs):
-            for j in range(len(df)):
-                self.lsh.add_fingerprint(df.iloc[j].fingerprint, f'{i}_{j}')
+    def _add_fingerprints_to_lsh(self, df):
+        domain = self._get_domain_of_site(df)
+        for i in range(len(df)):
+            row = df.iloc[i]
+            has_lead = True if df.iloc[i].lead != '' else False
+            self.lsh.add_fingerprint(row.fingerprint, f'{domain}_{row.uuid}_{row.cc_date}_{has_lead}')
 
     def filter_by_number_of_sentences(self, df):
         return df[df['article'].map(Tokenizer.count_sentences) > self.config.min_article_sentences]
