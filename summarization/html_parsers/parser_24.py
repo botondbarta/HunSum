eadf27abbebe0d8b6030b0345718adec60ed7593
dorinapetra <dorinapetra@gmail.com>
2022-09-14 14:44:41 +0200

    remove tags

--- summarization/html_parsers/parser_24.py
+++ summarization/html_parsers/parser_24.py
@@ -85,5 +85,7 @@ class Parser24(ParserBase):
         to_remove.extend(soup.find_all('div', class_='ad-container'))
         to_remove.extend(soup.find_all('div', class_='central-wp-gallery-wrapper'))
         to_remove.extend(soup.find_all(lambda t: t and 'central-wp-gallery' in t))
+        to_remove.extend(soup.find_all('div', class_='survey'))
+        to_remove.extend(soup.find_all('iframe'))
 
         return to_remove
