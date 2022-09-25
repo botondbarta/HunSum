771a8ae5e3f7eeb1faa461f736289d48dbf3e97a
Botond Barta <b.botond25@gmail.com>
2022-09-25 18:39:13 +0200

    fix typo from copypasta

--- summarization/entrypoints/clean.py
+++ summarization/entrypoints/clean.py
@@ -5,7 +5,7 @@ from summarization.preprocess.article_cleaner import ArticleCleaner
 
 @click.command()
 @click.argument('config_path')
-@click.option('--sites', default='all', help='Sites to scrape, separated by commas')
+@click.option('--sites', default='all', help='Sites to clean, separated by commas')
 def main(config_path, sites):
     cleaner = ArticleCleaner(config_path)
     cleaner.clean_articles(sites)
