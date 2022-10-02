7d70c1446c41ff49c2cfb18243c5d81efc9f1908
dorinapetra <dorinapetra@gmail.com>
2022-10-02 14:32:26 +0200

    add training entrypoint

---
+++ summarization/entrypoints/train.py
@@ -0,0 +1,19 @@
+import click
+
+from summarization.models.bert2bert import Bert2Bert
+from summarization.models.mt5 import MT5
+
+
+@click.command()
+@click.argument('model_type')
+@click.argument('config_path')
+def main(model_type, config_path):
+    if model_type == 'mt5':
+        model = MT5(config_path)
+    else:
+        model = Bert2Bert(config_path)
+    model.full_train()
+
+
+if __name__ == '__main__':
+    main()
