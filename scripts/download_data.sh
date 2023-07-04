7b2c0554d19e5a98bc00f0ceba459d8b05afc5a7
Botond <b.botond25@gmail.com>
2023-07-04 22:37:33 +0200

    updated data downloader to make it work with the new cc_corpus

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -16,28 +16,37 @@ do
 
   echo "Downloading $SITE from CommonCrawl ($URL)"
 
-  python "$CC_CORPUS_SCRIPT_PATH"/get_indexfiles.py \
-    -q $URL \
-    -o "$CC_OUTPUT_PATH/$SITE/cc_index" \
-    -l "$SITE.log" \
-    -m 5
+  segments=("CC-MAIN-2023-23" "CC-MAIN-2023-14" "CC-MAIN-2023-06" "CC-MAIN-2022-49" "CC-MAIN-2022-40" "CC-MAIN-2022-33" "CC-MAIN-2022-27" "CC-MAIN-2022-21" "CC-MAIN-2022-05" "CC-MAIN-2021-49" "CC-MAIN-2021-43" "CC-MAIN-2021-39" "CC-MAIN-2021-31" "CC-MAIN-2021-25" "CC-MAIN-2021-21" "CC-MAIN-2021-17" "CC-MAIN-2021-10" "CC-MAIN-2021-04" "CC-MAIN-2020-50" "CC-MAIN-2020-45" "CC-MAIN-2020-40" "CC-MAIN-2020-34" "CC-MAIN-2020-29" "CC-MAIN-2020-24" "CC-MAIN-2020-16" "CC-MAIN-2020-10" "CC-MAIN-2020-05" "CC-MAIN-2019-51" "CC-MAIN-2019-47" "CC-MAIN-2019-43" "CC-MAIN-2019-39" "CC-MAIN-2019-35" "CC-MAIN-2019-30" "CC-MAIN-2019-26" "CC-MAIN-2019-22" "CC-MAIN-2019-18" "CC-MAIN-2019-13" "CC-MAIN-2019-09" "CC-MAIN-2019-04" "CC-MAIN-2018-51" "CC-MAIN-2018-47" "CC-MAIN-2018-43" "CC-MAIN-2018-39" "CC-MAIN-2018-34" "CC-MAIN-2018-30" "CC-MAIN-2018-26" "CC-MAIN-2018-22" "CC-MAIN-2018-17" "CC-MAIN-2018-13" "CC-MAIN-2018-09" "CC-MAIN-2018-05" "CC-MAIN-2017-51" "CC-MAIN-2017-47" "CC-MAIN-2017-43" "CC-MAIN-2017-39" "CC-MAIN-2017-34" "CC-MAIN-2017-30" "CC-MAIN-2017-26" "CC-MAIN-2017-22" "CC-MAIN-2017-17" "CC-MAIN-2017-13" "CC-MAIN-2017-09" "CC-MAIN-2017-04" "CC-MAIN-2016-50" "CC-MAIN-2016-44" "CC-MAIN-2016-40" "CC-MAIN-2016-36" "CC-MAIN-2016-30" "CC-MAIN-2016-26" "CC-MAIN-2016-22" "CC-MAIN-2016-18" "CC-MAIN-2016-07" "CC-MAIN-2015-48" "CC-MAIN-2015-40" "CC-MAIN-2015-35" "CC-MAIN-2015-32" "CC-MAIN-2015-27" "CC-MAIN-2015-22" "CC-MAIN-2015-18" "CC-MAIN-2015-14" "CC-MAIN-2015-11" "CC-MAIN-2015-06" "CC-MAIN-2014-52" "CC-MAIN-2014-49" "CC-MAIN-2014-42" "CC-MAIN-2014-41" "CC-MAIN-2014-35" "CC-MAIN-2014-23" "CC-MAIN-2014-15" "CC-MAIN-2014-10" "CC-MAIN-2013-48" "CC-MAIN-2013-20")
+
+  # Loop through each segment and run the script
+  for segment in "${segments[@]}"
+  do
+    python "$CC_CORPUS_SCRIPT_PATH"/get_indexfiles.py \
+      -p $URL \
+      -o "$CC_OUTPUT_PATH/$SITE/cc_index/" \
+      -c "$segment"
+  done
 
   echo 'Filtering index'
   python "$CC_CORPUS_SCRIPT_PATH"/filter_index.py \
     "$CC_OUTPUT_PATH/$SITE/cc_index/" \
     "$CC_OUTPUT_PATH/$SITE/cc_index_filtered/" \
     -a "$ALLOWED_MIMES_PATH" \
-    -P 12
+    -P 15
 
   echo 'Deduplicating index'
   python "$CC_CORPUS_SCRIPT_PATH"/deduplicate_index_urls.py \
     -i "$CC_OUTPUT_PATH/$SITE/cc_index_filtered/" \
     -o "$CC_OUTPUT_PATH/$SITE/cc_index_dedup/"
 
-  echo 'Downloading pages'
+  mkdir "$CC_OUTPUT_PATH/$SITE/temp"
   python "$CC_CORPUS_SCRIPT_PATH"/download_pages.py \
-    -o "$CC_OUTPUT_PATH/$SITE/cc_downloaded" \
+    "$CC_OUTPUT_PATH/$SITE/cc_index_dedup/*.gz" \
+    "$CC_OUTPUT_PATH/$SITE/cc_index_output" \
+    "$CC_OUTPUT_PATH/$SITE/cc_downloaded" \
+    "$CC_OUTPUT_PATH/$SITE/error.txt" \
+    -t "$CC_OUTPUT_PATH/$SITE/temp/" \
     -e warc.gz \
-    -i "$CC_OUTPUT_PATH/$SITE/cc_index_dedup/*.gz"
+    -P 15
 
 done < "$1"
