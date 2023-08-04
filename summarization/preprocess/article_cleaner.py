707258936eeb6e326f4691ac4e6505d82ae8cf85
Botond Barta <b.botond25@gmail.com>
2023-08-04 15:34:00 +0200

    remove identical articles

--- summarization/preprocess/article_cleaner.py
+++ summarization/preprocess/article_cleaner.py
@@ -32,7 +32,8 @@ class ArticleCleaner:
         df_site = df_site[df_site['lead'] != '']
         logger.info(f'Cleaning {site}, size: {len(df_site)}')
 
-        # drop articles where lead is longer than article
+        df_site = self._dedup_by_url_lead_and_article(df_site, logger)
+
         df_site = df_site[df_site['article'].str.len() > df_site['lead'].str.len()]
         logger.info(f'Dropped articles where lead is longer than article, size: {len(df_site)}')
 
@@ -67,6 +68,15 @@ class ArticleCleaner:
         domain = get_domain_of_df_site(df_site)
         df_site.to_json(f'{self.config.clean_out_dir}/{domain}.jsonl.gz', orient='records',
                         lines=True, compression='gzip')
+
+    def _dedup_by_url_lead_and_article(self, df, logger):
+        df['url_end'] = df.apply(lambda row: row['url'].partition('://')[2], axis=1)
+        df = df.drop_duplicates(subset=['url_end'])
+        logger.info(f'Deduplicated by url, size: {len(df)}')
+        df = df.drop_duplicates(subset=['article', 'lead'])
+        logger.info(f'Deduplicated by article and lead, size: {len(df)}')
+        df = df.drop('url_end', axis=1)
+        return df
 
     def _drop_non_hungarian_sentences(self, df):
         return df[df['article']
