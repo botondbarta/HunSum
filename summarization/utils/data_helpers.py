5a07970cd376a4912d2a85e2ea2deff968fe5989
Botond Barta <b.botond25@gmail.com>
2023-11-02 14:50:36 +0100

    use Pool during deduplication instead of ThreadPool

--- summarization/utils/data_helpers.py
+++ summarization/utils/data_helpers.py
@@ -1,30 +1,16 @@
-from multiprocessing import Pool, cpu_count
-from multiprocessing.pool import ThreadPool
+from multiprocessing import Pool
 from os import path, mkdir
 from typing import List
 
 import numpy as np
 import pandas as pd
-from tqdm import tqdm
+
 
 def parallelize_df_processing(df, func, num_cores=4, num_partitions=20):
     df_split = np.array_split(df, num_partitions)
     with Pool(num_cores) as pool:
         df = pd.concat(pool.map(func, df_split))
     return df
-
-
-def parallelize_df_processing_progress_bar(df, func, num_partitions=None):
-    if num_partitions is None:
-        num_partitions = cpu_count()
-
-    partitions = np.array_split(df, num_partitions)
-    processed_partitions = []
-
-    with ThreadPool(num_partitions) as pool:
-        for processed_partition in tqdm(pool.imap_unordered(func, partitions), total=num_partitions):
-            processed_partitions.append(processed_partition)
-    return pd.concat(processed_partitions)
 
 
 def make_dir_if_not_exists(directory):
